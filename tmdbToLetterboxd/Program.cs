b2fb849000b150141a3975bb0989e40541ed0699
zSavT <francesco.saverio.cassano@gmail.com>
2026-08-23 14:19:03 +0200

    Inizio

---
+++ tmdbToLetterboxd/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace tool
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("Test");
+            var client = new HttpClient();
+            //client.BaseAddress = new Uri("");
+        }
+    }
+
+    class Response
+    {
+
+    }
+
+
+}
